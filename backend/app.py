342181bf1f9b74e5c9df97facd1639313b9dd548
Yati Phoo Pwint Thwe <yatiphoopwint2003@gmail.com>
2025-12-11 12:59:40 +0800

    final changes

--- backend/app.py
+++ backend/app.py
@@ -37,14 +37,6 @@ jwt = JWTManager(app)
 migrate = Migrate(app, db)
 
 
-# --- Auto-seed users if table is empty ---
-from models import User
-from seed_db import seed_users  # your seeding script
-
-with app.app_context():
-    if User.query.count() == 0:
-        print("No users found. Seeding database...")
-        seed_users()
 
 # --- Register API routes ---
 from routes import register_routes
